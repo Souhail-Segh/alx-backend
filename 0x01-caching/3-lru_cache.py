96351e0a0bf3333e0c6498a0881b080d4ce6d7a4
Souhail-Segh <souhail.seghds.ds@gmail.com>
2024-10-30 22:02:30 +0000

    3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -32,5 +32,6 @@ class LRUCache(BaseCaching):
         """
         if key is not None:
             if key in self.cache_data:
+                self.cache_data.move_to_end(key, last=False)
                 return self.cache_data[key]
         return None
