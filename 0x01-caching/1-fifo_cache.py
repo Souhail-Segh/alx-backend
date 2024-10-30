32f3ddd9446650f66b0c3a75779bd78c69b5f549
Souhail-Segh <souhail.seghds.ds@gmail.com>
2024-10-30 21:11:17 +0000

    1-fifo_cache.py

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -21,7 +21,7 @@ class FIFOCache(BaseCaching):
         """put key and item in the dict
         """
         if key is not None and item is not None:
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
                 discard,_ = self.cache_data.popitem(last=False)
                 print(f'DISCARD: {discard}')
             self.cache_data[key] = item
